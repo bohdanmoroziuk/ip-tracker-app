4c5164a0422e7e19699171ee58cfdc98c4cb7cae
Bogdan Morozyuk <bogdan7morozyuk@gmail.com>
2021-07-10 12:37:51 +0300

    feat: update document title

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,9 @@ const routes = [
   {
     path: '/',
     name: 'Home',
+    meta: {
+      title: 'IP Address Tracker',
+    },
     component: () => import('@/views/Home.vue'),
   },
 ];
@@ -11,6 +14,12 @@ const routes = [
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes,
+});
+
+router.beforeEach((to, _from, next) => {
+  document.title ??= to.meta?.title;
+
+  next();
 });
 
 export default router;
