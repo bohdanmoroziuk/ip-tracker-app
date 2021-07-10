61c168b56e7d8f2ac6a5ab8c025d8ccaf312acab
Bogdan Morozyuk <bogdan7morozyuk@gmail.com>
2021-07-10 13:09:48 +0300

    feat(views.home): add search form

---
+++ src/components/SearchForm.vue
@@ -0,0 +1,34 @@
+<template>
+  <form
+    class="search-form flex"
+    @submit.prevent="handleSubmit"
+  >
+    <input
+      class="flex-1 py-3 px-2 rounded-tl-md rounded-bl-md focus:outline-none"
+      type="text"
+      name="searchTerm"
+      id="searchTerm"
+      placeholder="Search for any IP address or leave empty to get your ip info"
+      v-model.trim="searchTerm"
+    />
+    <button class="bg-black text-white px-4 rounded-tr-md rounded-br-md">
+      <i class="fas fa-chevron-right"></i>
+    </button>
+  </form>
+</template>
+
+<script>
+export default {
+  name: 'SearchForm',
+  data() {
+    return {
+      searchTerm: '',
+    };
+  },
+  methods: {
+    handleSubmit() {
+      this.$emit('search', this.searchTerm);
+    },
+  },
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,22 @@
 <template>
-  <div class="home">
-    Home
+  <div class="home flex flex-col h-screen max-h-screen">
+    <div class="flex justify-center relative px-4 pb-32 bg-hero-pattern bg-cover">
+      <div class="w-full max-w-screen-sm">
+        <h1 class="text-white text-center text-3xl pb-4">IP Address Tracker</h1>
+
+        <search-form />
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import SearchForm from '@/components/SearchForm.vue';
+
 export default {
   name: 'Home',
+  components: {
+    SearchForm,
+  },
 };
 </script>
